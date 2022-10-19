a3a658e957d0a2b45952ed9ede0cbc663804ea45
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-10-19 22:10:48 +0200

    email ui changes

--- src/test/java/guestbook/GuestbookEntryUnitTests.java
+++ src/test/java/guestbook/GuestbookEntryUnitTests.java
@@ -30,18 +30,18 @@ class GuestbookEntryUnitTests {
 	void rejectsEmptyName() {
 
 		assertThatExceptionOfType(IllegalArgumentException.class)//
-				.isThrownBy(() -> new GuestbookEntry("", "May the 4th be with you!"));
+				.isThrownBy(() -> new GuestbookEntry("", "May the 4th be with you!", "foo@foo"));
 	}
 
 	@Test
 	void rejectsEmptyText() {
 
 		assertThatExceptionOfType(IllegalArgumentException.class)//
-				.isThrownBy(() -> new GuestbookEntry("Ollie", ""));
+				.isThrownBy(() -> new GuestbookEntry("Ollie", "", "foo@foo"));
 	}
 
 	@Test
 	void setsCreationDate() {
-		assertThat(new GuestbookEntry("Ollie", "May the 4th be with you!").getDate()).isNotNull();
+		assertThat(new GuestbookEntry("Ollie", "May the 4th be with you!", "foo@foo").getDate()).isNotNull();
 	}
 }
