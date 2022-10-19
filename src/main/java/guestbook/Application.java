650f6ab18d942178afa662e3c3d841855947244e
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-10-19 21:44:41 +0200

    added email to guestbookform

--- src/main/java/guestbook/Application.java
+++ src/main/java/guestbook/Application.java
@@ -60,12 +60,12 @@ public class Application {
 		return args -> {
 
 			Stream.of( //
-					new GuestbookEntry("H4xx0r", "first!!!"), //
-					new GuestbookEntry("Arni", "Hasta la vista, baby"), //
+					new GuestbookEntry("H4xx0r", "first!!!", "foo@foo"), //
+					new GuestbookEntry("Arni", "Hasta la vista, baby", "foo@foo"), //
 					new GuestbookEntry("Duke Nukem",
-							"It's time to kick ass and chew bubble gum. And I'm all out of gum."), //
+							"It's time to kick ass and chew bubble gum. And I'm all out of gum.", "foo@foo"), //
 					new GuestbookEntry("Gump1337",
-							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) //
+							"Mama always said life was like a box of chocolates. You never know what you're gonna get.", "foo@foo")) //
 					.forEach(guestbook::save);
 		};
 	}
--- src/main/java/guestbook/GuestbookForm.java
+++ src/main/java/guestbook/GuestbookForm.java
@@ -31,6 +31,7 @@ class GuestbookForm {
 
 	private final @NotBlank String name;
 	private final @NotBlank String text;
+	private final @NotBlank String email;
 
 	/**
 	 * Creates a new {@link GuestbookForm} with the given name and text. Spring Framework will use this constructor to
@@ -42,10 +43,11 @@ class GuestbookForm {
 	 * @param name the value to bind to {@code name}
 	 * @param text the value to bind to {@code text}
 	 */
-	public GuestbookForm(String name, String text) {
+	public GuestbookForm(String name, String text, String email) {
 
 		this.name = name;
 		this.text = text;
+		this.email=email;
 	}
 
 	/**
@@ -76,7 +78,9 @@ class GuestbookForm {
 	 * @return the newly created {@link GuestbookEntry}
 	 * @throws IllegalArgumentException if you call this on an instance without the name and text actually set.
 	 */
+
+	public String getEmail() {return email;}
 	GuestbookEntry toNewEntry() {
-		return new GuestbookEntry(getName(), getText());
+		return new GuestbookEntry(getName(), getText(), getEmail());
 	}
 }
